e8a11f90402f03e11ceb1d9036d56c0c5dff0055
boloto51 <boloto51@mail.ru>
2018-04-22 12:59:12 +0300

    Aaa Snake from Internet

---
+++ Console_Snake/Console_Snake_from_Internet/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//http://www.cyberforum.ru/csharp-beginners/thread665111.html
+
+namespace Console_Snake_from_Internet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var snake = new Snake(x: 5, y: 5, size: 7);
+            Print(snake);
+            while (true)
+            {
+                var key = Console.ReadKey().Key;
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow:
+                        snake.Direction = Direction.Up;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        snake.Direction = Direction.Down;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        snake.Direction = Direction.Left;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        snake.Direction = Direction.Right;
+                        break;
+                }
+                snake.Move(1);
+                Print(snake);
+            }
+        }
+
+        private static void Print(Snake snake)
+        {
+            Console.Clear();
+            for (var i = 0; i < snake.Size; i++)
+            {
+                Console.SetCursorPosition(snake[i].X, snake[i].Y);
+                Console.Write(i == 0 ? "*" : "0");
+            }
+        }
+    }
+
+    internal class Snake
+    {
+        private readonly List<Segment> segments;
+
+        public Snake(int x, int y, uint size)
+        {
+            if (size == 0)
+                throw new ArgumentException("Размер змеи не может быть равным 0", "size");
+            segments = new List<Segment>();
+            for (var i = 0; i < size; i++)
+                segments.Add(new Segment(x, y + i));
+            Direction = Direction.Up;
+        }
+
+        public Direction Direction { get; set; }
+
+        public int Size
+        {
+            get { return segments.Count; }
+        }
+
+        public Segment this[int index]
+        {
+            get { return segments[index]; }
+        }
+
+        public void Move(int speed)
+        {
+            int x = 0, y = 0;
+            switch (Direction)
+            {
+                case Direction.Up:
+                    y = -speed;
+                    break;
+                case Direction.Down:
+                    y = speed;
+                    break;
+                case Direction.Left:
+                    x = -speed;
+                    break;
+                case Direction.Right:
+                    x = speed;
+                    break;
+            }
+            for (var i = Size - 1; i >= 1; i--)
+            {
+                segments[i].X = segments[i - 1].X;
+                segments[i].Y = segments[i - 1].Y;
+            }
+            segments[0].X += x;
+            segments[0].Y += y;
+        }
+    }
+
+    internal class Segment
+    {
+        public Segment(int x = 0, int y = 0)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    internal enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
---
+++ Console_Snake/Console_Snake_from_Internet/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Console_Snake_from_Internet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Console_Snake_from_Internet")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("d7a3d03c-a107-46cc-a993-68ec3f946002")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
