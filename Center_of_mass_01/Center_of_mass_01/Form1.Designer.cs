8c80bbf7b68091157ea045e072627e78391f1d8f
boloto51 <boloto51@mail.ru>
2018-05-12 22:51:25 +0300

    Centre of Mass 01

---
+++ Center_of_mass_01/Center_of_mass_01/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Center_of_mass_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnCentre = new System.Windows.Forms.Button();
+            this.tbListPoints = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(13, 219);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Генерация";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnCentre
+            // 
+            this.btnCentre.Location = new System.Drawing.Point(95, 219);
+            this.btnCentre.Name = "btnCentre";
+            this.btnCentre.Size = new System.Drawing.Size(75, 23);
+            this.btnCentre.TabIndex = 2;
+            this.btnCentre.Text = "Центр масс";
+            this.btnCentre.UseVisualStyleBackColor = true;
+            this.btnCentre.Click += new System.EventHandler(this.btnCentre_Click);
+            // 
+            // tbListPoints
+            // 
+            this.tbListPoints.Location = new System.Drawing.Point(219, 13);
+            this.tbListPoints.Multiline = true;
+            this.tbListPoints.Name = "tbListPoints";
+            this.tbListPoints.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbListPoints.Size = new System.Drawing.Size(200, 200);
+            this.tbListPoints.TabIndex = 3;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(176, 218);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Очистка";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 262);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.tbListPoints);
+            this.Controls.Add(this.btnCentre);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnCentre;
+        private System.Windows.Forms.TextBox tbListPoints;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Center_of_mass_01/Center_of_mass_01/Form1.cs
@@ -0,0 +1,87 @@
+﻿// https://msdn.microsoft.com/ru-ru/library/wt06kxfd(v=vs.110).aspx
+// https://stackoverflow.com/questions/8536958/how-to-add-a-line-to-a-multiline-textbox
+// http://life-prog.ru/2_7313_metodi-zalivki.html
+// https://msdn.microsoft.com/ru-ru/library/2t63kk0t(v=vs.110).aspx
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Center_of_mass_01
+{
+    public partial class Form1 : Form
+    {
+        static int numberOfPoints = 10;
+        Random rnd = new Random();
+        int[] x = new int[numberOfPoints];
+        int[] y = new int[numberOfPoints];
+        int[] m = new int[numberOfPoints];
+        int x_centre, y_centre;
+        int m_sum;
+        Graphics graph;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            graph = pictureBox1.CreateGraphics();
+            //g.DrawLine(new Pen(Brushes.Red, 4), new Point(10, 10), new Point(100, 100));
+            for (int i = 0; i < numberOfPoints; i++)
+            {
+                x[i] = rnd.Next(0, pictureBox1.Width);
+                y[i] = rnd.Next(0, pictureBox1.Height);
+                m[i] = rnd.Next(21);
+
+                tbListPoints.Text += "x[" + i + "]=" + $"{x[i]}"
+                    + "\ty[" + i + "]=" + $"{y[i]}"
+                    + "\tm[" + i + "]=" + $"{m[i]}" + "\r\n";
+
+                Pen pen = new Pen(Color.Blue, 2);
+                graph.DrawEllipse(pen, (int)(x[i] - m[i] / 2), (int)(pictureBox1.Height - y[i] - m[i] / 2), m[i], m[i]);
+                Brush brush = Brushes.Yellow;
+                graph.FillEllipse(brush, (int)(x[i] - m[i] / 2), (int)(pictureBox1.Height - y[i] - m[i] / 2), m[i], m[i]);
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            tbListPoints.Clear();
+        }
+
+        private void btnCentre_Click(object sender, EventArgs e)
+        {
+            x_centre = 0;
+            y_centre = 0;
+            m_sum = 0;
+
+            for (int i = 0; i < numberOfPoints; i++)
+            {
+                x_centre += x[i] * m[i];
+                y_centre += y[i] * m[i];
+                m_sum += m[i];
+            }
+
+            x_centre = (int)(x_centre / m_sum);
+            y_centre = (int)(y_centre / m_sum);
+            tbListPoints.Text += "x_cntr=" + $"{x_centre}"
+                + "\ty_cntr=" + $"{y_centre}"
+                + "\tm_sum=" + $"{m_sum}" + "\r\n";
+
+            Pen pen_centre = new Pen(Color.Red, 2);
+            Brush brush_centre = Brushes.Red;
+            graph.DrawEllipse(pen_centre, x_centre - 5, pictureBox1.Height - y_centre - 5, 10, 10);
+            graph.FillEllipse(brush_centre, x_centre - 5, pictureBox1.Height - y_centre - 5, 10, 10);
+        }
+    }
+}
